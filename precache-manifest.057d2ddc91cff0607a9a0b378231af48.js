43b19faac8dfabf38a4c04a7092d1156cd8dd3e3
Kari Burt <kandreahburt@gmail.com>
2020-04-11 03:59:40 -0500

    Updates

---
+++ precache-manifest.057d2ddc91cff0607a9a0b378231af48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a0332f25dc28437988430f2e16f7bf",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "c582342bc2b74f6315b3",
+    "url": "/pokedex/static/css/main.7790b6fe.chunk.css"
+  },
+  {
+    "revision": "bd5ce98e099165808ed7",
+    "url": "/pokedex/static/js/2.9ae91970.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.9ae91970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c582342bc2b74f6315b3",
+    "url": "/pokedex/static/js/main.4c93f947.chunk.js"
+  },
+  {
+    "revision": "601f8d113175821311bd",
+    "url": "/pokedex/static/js/runtime-main.0ec6b405.js"
+  },
+  {
+    "revision": "6a906af4a9eddf84772e827f4a637464",
+    "url": "/pokedex/static/media/logo.6a906af4.png"
+  }
+]);
